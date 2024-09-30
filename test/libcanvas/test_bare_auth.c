1c6dcd87c2cddfc3a27d8500bcee8cb3d7f1747d
Advaith Menon <83835839+advaithm582@users.noreply.github.com>
2024-09-30 12:41:47 -0400

    Update to ANSI C comment
    * Update to ANSI C comment

--- test/libcanvas/test_bare_auth.c
+++ test/libcanvas/test_bare_auth.c
@@ -7,7 +7,7 @@
  */
 void test_canvas_init_token_bare() {
     AUTH *canvas_auth = canvas_init_token_bare("qwerty1234ASDFzxcvbnm", 632);
-    // now check if mode and token equal what you want it to be.
+    /* now check if mode and token equal what you want it to be. */
     g_assert_cmpstr(canvas_auth->token, ==, "qwerty1234ASDFzxcvbnm");
     g_assert(canvas_auth->expires == 632UL);
 }
